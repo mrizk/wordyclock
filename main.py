d1cb067a91d477aa6731acb69eed97ad50354ff7
Al Moataz Rizk <mor@vioma.de>
2019-09-22 23:16:00 +0200

    only clear on update

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ if __name__ == '__main__':
         wordClock.set_time_words()
         wordClock.corner_wipe(50)
         while True:
-            wordClock.display(words.indecies_from_matrix(0, constants.CLOCK_WIDTH-1, 0, constants.CLOCK_HEIGHT-1), Color(0, 0, 0))
             wordClock.set_time_words()
             if wordClock.clock_updated:
+                wordClock.display(words.indecies_from_matrix(0, constants.CLOCK_WIDTH-1, 0, constants.CLOCK_HEIGHT-1), Color(0, 0, 0))
                 wordClock.display(wordClock.time_words, Color(255, 255, 255))
             time.sleep(50 / 1000.0)
 

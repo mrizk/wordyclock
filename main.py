eb3d520ec11501d48c3ebcfe7327c7ae79e6875e
Al Moataz Rizk <mor@vioma.de>
2019-09-22 23:14:03 +0200

    only update on update

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ if __name__ == '__main__':
         while True:
             wordClock.display(words.indecies_from_matrix(0, constants.CLOCK_WIDTH-1, 0, constants.CLOCK_HEIGHT-1), Color(0, 0, 0))
             wordClock.set_time_words()
-            wordClock.display(wordClock.time_words, Color(255, 255, 255))
-            time.sleep(1000 / 1000.0)
+            if wordClock.clock_updated:
+                wordClock.display(wordClock.time_words, Color(255, 255, 255))
+            time.sleep(50 / 1000.0)
 
 
     except KeyboardInterrupt:

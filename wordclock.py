a78b12973321ba41450bc5e961ededeb9e4f4e38
Al Moataz Rizk <mor@vioma.de>
2019-09-22 21:19:57 +0200

    fix

--- wordclock.py
+++ wordclock.py
@@ -55,7 +55,7 @@ class WordClock:
             for y in range(0, height):
                 for x in range(0, width):
                     if x <= row_index and x >= row_index - self.corner_wipe_width:
-                        self.strip.setPixelColor(words.indecies_from_matrix(x, x, y, y)[0], self.wheel((((index * 255) / (width * height)) + color_index) & 255))
+                        self.strip.setPixelColor(words.indecies_from_matrix(x, x, y, y)[0], self.wheel((((x * 255) / (width * height)) + color_index) & 255))
                     else:
                         self.strip.setPixelColor(words.indecies_from_matrix(x, x, y, y)[0], Color(0, 0, 0))
                 row_index -= 1
@@ -64,6 +64,6 @@ class WordClock:
             color_index -= 1
             if color_index == 0:
                 color_index = 255
-            if index == (width * height) + 3:
+            if index == width + height + 3:
                 index = 0
             time.sleep(wait_ms / 1000.0)
